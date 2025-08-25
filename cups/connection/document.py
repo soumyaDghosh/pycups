8b801b571a11b4f76f932e0c933180d2002c122d
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-08-25 12:11:50 +0530

    connection/documents: new APIs related to cups documents
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

---
+++ cups/connection/document.py
@@ -0,0 +1,42 @@
+from cups.types.cups import cupsDest, cupsDestInfo, cupsOption
+from cups.enums.http import HttpStatus
+from cups.enums.ipp import IPPStatus
+from .base import _Base, _cups
+from typing import Any, Dict
+
+_lib = _cups.lib
+
+
+class DocumentMixin(_Base):
+    http: Any = None
+
+    def startDestDocument(
+        self,
+        dest: cupsDest,
+        dest_info: cupsDestInfo,
+        job_id: int,
+        docname: str,
+        format: str,
+        options: Dict[str, cupsOption],
+        last_document: bool,
+    ) -> HttpStatus:
+        status = _lib.cupsStartDestDocument(
+            self.http,
+            dest.ffi_value,
+            dest_info.ffi_value,
+            job_id,
+            docname.encode(),
+            format.encode(),
+            len(options),
+            cupsOption.to_cffi_list(options),
+            last_document,
+        )
+        return HttpStatus(status)
+
+    def finishDestDocument(self, dest: cupsDest, dinfo: cupsDestInfo) -> IPPStatus:
+        return IPPStatus(
+            _lib.cupsFinishDestDocument(self.http, dest.ffi_value, dinfo.ffi_value)
+        )
+
+    def writeRequestData(self, buffer: str, len: int) -> HttpStatus:
+        return HttpStatus(_lib.cupsWriteRequestData(self.http, buffer.encode(), len))
