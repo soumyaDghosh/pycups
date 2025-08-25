cfc5c4d59bd5442f7aa3852c070d083f34d3e6fa
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-08-25 11:51:35 +0530

    enums: impl new enums for http, job and media
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

---
+++ cups/enums/jobs.py
@@ -0,0 +1,10 @@
+from enum import IntFlag
+from cups import _cups
+
+_lib = _cups.lib
+
+
+class WhichJobs(IntFlag):
+    ACTIVE = _lib.CUPS_WHICHJOBS_ACTIVE
+    ALL = _lib.CUPS_WHICHJOBS_ALL
+    COMPLETED = _lib.CUPS_WHICHJOBS_COMPLETED
---
+++ cups/enums/media.py
@@ -0,0 +1,12 @@
+from enum import IntFlag
+from cups import _cups
+
+_lib = _cups.lib
+
+
+class CUPSMediaFlags(IntFlag):
+    BORDERLESS = _lib.CUPS_MEDIA_FLAGS_BORDERLESS
+    DEFAULT = _lib.CUPS_MEDIA_FLAGS_DEFAULT
+    DUPLEX = _lib.CUPS_MEDIA_FLAGS_DUPLEX
+    EXACT = _lib.CUPS_MEDIA_FLAGS_EXACT
+    READY = _lib.CUPS_MEDIA_FLAGS_READY
