efafa65539793dfec2010d273b2e78192f341141
Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>
2025-08-28 12:09:13 +0530

    enums/encoding: new enc cupsEncoding
    Signed-off-by: Soumyadeep Ghosh <soumyadeepghosh2004@zohomail.in>

--- cups/enums/__init__.py
+++ cups/enums/__init__.py
@@ -1,9 +1,11 @@
 from .cups import CUPSDestFlags
 from .http import HttpEncryption
 from .ipp import IPPOp, IPPRes, IPPState, IPPStatus, IPPTag
+from .encoding import CUPSEncoding
 
 __all__ = [
     "CUPSDestFlags",
+    "CUPSEncoding",
     "HttpEncryption",
     "IPPOp",
     "IPPRes",
